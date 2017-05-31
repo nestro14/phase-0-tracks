9d84c0c6895f59eb7753a7973997c24da5b2e277
Nestor Ceron <nestor_ceron@yahoo.com>
2017-05-31 12:04:18 -0700

    Set hamster variables from user input

---
+++ ruby/hamsters.rb
@@ -0,0 +1,21 @@
+hamster_name = ""
+volume_level = 0
+fur_color = ""
+good_candidate = ""
+estimated_age = nil
+
+puts "hamster's name: "
+hamster_name = gets.chomp
+
+puts "volume level(1-10): "
+volume_level = gets.chomp.to_i
+
+puts "Fur Color: "
+fur_color = gets.chomp
+
+puts "Good candidate for adoption?(y/n)"
+good_candidate = gets.chomp
+
+puts "Estimated age: "
+estimated_age = gets.chomp.to_i
+
