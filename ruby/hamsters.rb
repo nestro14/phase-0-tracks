26a3f80f0500a00346460e9fc42bc5a84f6e5c12
Nestor Ceron <nestor_ceron@yahoo.com>
2017-05-31 12:19:48 -0700

    Add presentable data output

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -13,9 +13,15 @@ volume_level = gets.chomp.to_i
 puts "Fur Color: "
 fur_color = gets.chomp
 
-puts "Good candidate for adoption?(y/n)"
+puts "The hamster is a (good or bad) candidate for adoption?"
 good_candidate = gets.chomp
 
 puts "Estimated age: "
 estimated_age = gets.chomp.to_i
+
+puts "The hamster's name is #{hamster_name}."
+puts "#{hamster_name}'s noise level on a scale of 1-10 is #{volume_level}."
+puts "#{hamster_name} has #{fur_color} colored fur."
+puts "#{hamster_name} is a #{good_candidate} candidate for adoption."
+puts "#{hamster_name}'s estimated age is #{estimated_age}."
 
