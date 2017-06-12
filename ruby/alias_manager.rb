8a67b12f667115edad835310e8e8b11162bf0242
Nestor Ceron <nestor_ceron@yahoo.com>
2017-06-11 18:49:46 -0700

    Add method to create alias and pseudocode

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,22 @@
 # Release 0: Attempt a Tricky Algorithm
-#pseudocode
 
-#1. Swapping the first and last name.
+##pseudocode
+# Create constants for vowels and consonats
+# split up first and last name into array
+# swap first and last name
+# split up names into characters
+# create a method to validate if the char is a vowel or consonant
+# create a method that check whether the character needs to be swapped
+# create a method to return the next vowel
+# create a method to return the next consonant
+# iterate through characters in names and return the next character
+# join the characters together and first and last name
+# create method to put the alias all together
+# create a loop to ask user for names to be changed into an alias
+
+VOWELS = ['a','e','i','o','u']
+CONSONANTS = ('a'..'z').to_a.delete_if{|c| char_vowel?(c) == true}
+
 def swap_first_last_name(full_name)
   full_name.split(' ').reverse!
 end
@@ -12,11 +27,7 @@ end
 
 def swap_case?(char)
   letters = ('a'..'z').to_a
-  if letters.include?(char)
-    false
-  else
-    true
-  end
+  letters.include?(char) ? false : true
 end
 
 def next_vowel(char)
@@ -43,14 +54,14 @@ def next_consonant(char)
   end
 end
 
-def make_fake_name(spy_name)
+def convert_name_to_alias(spy_name)
   spy_name.chars.map{|char| char_vowel?(char) ? next_vowel(char) : next_consonant(char) }.join
 end
 
-VOWELS = ['a','e','i','o','u']
-CONSONANTS = ('a'..'z').to_a.delete_if{|c| char_vowel?(c) == true}
-spy_name = "Felicia Torres"
+def create_alias(spy_name)
+  first_name, last_name = swap_first_last_name(spy_name)
+fake_name = convert_name_to_alias(first_name).concat(' ') + convert_name_to_alias(last_name)
+end
 
-first_name, last_name = swap_first_last_name(spy_name)
-fake_name = make_fake_name(first_name).concat(' ') + make_fake_name(last_name)
+p create_alias("Felicia Torres")
 
