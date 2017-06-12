a60fd5c65db785339fb29e40e87c9cd59ffa0909
Nestor Ceron <nestor_ceron@yahoo.com>
2017-06-12 15:12:52 -0700

    Add puppy class and methods

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -2,3 +2,22 @@
 # Initialize the puppy info - Instance variables
 #   - Name, color, size, etc
 # Define puppy methods like 'bark' and 'growl'
+
+class Puppy
+  attr_accessor :name, :color, :size
+
+  def initialize(name = '', color = '', size = '')
+      @name = name
+      @color = color
+      @size = size
+    end
+
+  def bark
+    puts "Woof! Woof!"
+  end
+
+  def growl
+    puts "Grrrrr! Grrrrr!"
+  end
+
+end
