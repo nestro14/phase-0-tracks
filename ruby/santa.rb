bdb5d2bee7050c13df761e5df18e8d11bafa39a3
Nestor Ceron <nestor_ceron@yahoo.com>
2017-06-12 15:48:14 -0700

    Add some diverse initializations

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,12 @@
 class Santa
 
-  def initialize
-    puts "Initializing Santa instance ..."
+  attr_accessor :gender, :ethnicity, :age
+
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -14,6 +19,18 @@ class Santa
 
 end
 
-claus = Santa.new
-claus.speak
-claus.eat_milk_and_cookies("Chocolate Chip")
+# Test code
+# claus = Santa.new
+# claus.speak
+# claus.eat_milk_and_cookies("Chocolate Chip")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese", "Chinese", "Hawaiian", "N/A", "Korean", "European", "Haitian"]
+
+10.times {santas << Santa.new(example_genders.sample, example_ethnicities.sample)}
+
+santas.each do |santa|
+  puts "This santa's gender and ethnicity is: #{santa.gender} and #{santa.ethnicity}"
+end
+
