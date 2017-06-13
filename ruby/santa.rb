a7a216a2bd1caadace66f8108d6e48b31e7cf7cb
Nestor Ceron <nestor_ceron@yahoo.com>
2017-06-13 12:40:41 -0700

    Add three attribute-changing methods to santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,20 +17,34 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
+  def celebrate_birthday
+    self.age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+    temp = @reindeer_ranking.delete_at(@reindeer_ranking.rindex(reindeer_name))
+    @reindeer_ranking.push(temp)
+  end
+
 end
 
-# Test code
-# claus = Santa.new
-# claus.speak
-# claus.eat_milk_and_cookies("Chocolate Chip")
+#Test code
+claus = Santa.new("Male", "Latino")
+claus.speak
+claus.eat_milk_and_cookies("Chocolate Chip")
+claus.celebrate_birthday
+claus.get_mad_at("Donner")
+p claus.age
+p claus.gender
+p claus
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese", "Chinese", "Hawaiian", "N/A", "Korean", "European", "Haitian"]
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese", "Chinese", "Hawaiian", "N/A", "Korean", "European", "Haitian"]
 
-10.times {santas << Santa.new(example_genders.sample, example_ethnicities.sample)}
+# 10.times {santas << Santa.new(example_genders.sample, example_ethnicities.sample)}
 
-santas.each do |santa|
-  puts "This santa's gender and ethnicity is: #{santa.gender} and #{santa.ethnicity}"
-end
+# santas.each do |santa|
+#   puts "This santa's gender and ethnicity is: #{santa.gender} and #{santa.ethnicity}"
+# end
 
