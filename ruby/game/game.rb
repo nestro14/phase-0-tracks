8451f86f2ee66464c67d8a7d69269b1bd62d8e30
Nestor Ceron <nestor_ceron@yahoo.com>
2017-06-18 10:19:50 -0700

    Add pseudocode for game class

---
+++ ruby/game/game.rb
@@ -0,0 +1,14 @@
+# Pseudocode
+# Define class name: GuessPhrase
+# Define class instance variables for guess_count, phrase, game_over?, and progress_of_guessed_phrase, guess_limit
+# Define methods:
+#   - update_phrase() => returns updated phrase based on user input
+#   - char_in_pharse?() => returns true if char is in phrase
+#   - display_result_message() => return winning/losing message
+#   - display_wrong_guess_count => displays wrong guess count
+#   - calculate_guess_limit => return guess lime fo 7 if phrase is <= 12 and 12 if guess limit is > 12
+# Prompt 1 user for phrase
+# Loop until game is over
+#   -Prompt another user to guess phrase buy entering 1 character
+#   -display update phrase and wrong guess count
+# diplay game result
