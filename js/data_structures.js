319a880901fd10bba89394f7905d262266d7e0e0
Nestor Ceron <nestor_ceron@yahoo.com>
2017-06-21 09:14:10 -0700

    Add two arrays names and colors

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ['blue', 'red', 'green', 'orange'];
+
+var names = ['lighting', 'thunder', 'flash', 'speedy'];
+
+names.push('fasty');
+colors.push('pink');
+
+console.log(colors);
+console.log(names);
