92e494e5b9e91d664372b2566ad88cd99be0e102
Nestor Ceron <nestor_ceron@yahoo.com>
2017-06-21 09:47:12 -0700

    Create new car objects with examples

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,28 @@ for (var i = colors.length - 1; i >= 0; i--) {
 }
 
 console.log(horse);
+
+// Car object
+
+function Car(type, model, year, color){
+  this.type = type;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+
+  this.honk = function(){
+    console.log("*Honk! Honk!*");
+  };
+  console.log("Car Assembly Complete.");
+}
+
+var car1 = new Car('Toyota', "Corolla", 2012, "blue");
+var car2 = new Car('Toyota', "Camry", 1998, "green");
+var car3 = new Car('Audi', "A4", 2003, "beige");
+
+car1.honk();
+car1.honk();
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
