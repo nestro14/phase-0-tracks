718cf38ba56269abf217d672a2b63f98ed115cbd
Nestor Ceron <nestor_ceron@yahoo.com>
2017-06-25 22:50:25 -0700

    Create function and driver code to generate random words and find the longest

--- js/algos.js
+++ js/algos.js
@@ -15,7 +15,7 @@ function longestPhrase(array){
   var longest = ''
   for(phrase in array){
     if (longest.length < array[phrase].length) {longest = array[phrase]};
-  }
+  };
   return longest;
 }
 
@@ -32,11 +32,35 @@ function hasKeyValuePair(object1, object2){
   return result;
 }
 
+function generateWords(x){
+  words = [];
+  characters = [];
+  for (var i = 1; i <= x; i++) {
+    randomLength = Math.floor((Math.random() * 10) + 1);
+    for (var j = 1; j <= randomLength; j++) {
+      characters.push(String.fromCharCode(Math.floor((Math.random() * 25) + 97)));
+    };
+    words.push(characters.join(''));
+    characters = []
+  };
+  return words;
+}
+// Math.floor((Math.random() * 10) + 1);
+
+// Math.floor((Math.random() * 57) + 65);
+
 
 // Driver Code
 // console.log(longestPhrase(testArray1));
 // console.log(longestPhrase(testArray2));
 // console.log(longestPhrase(testArray3));
 
-console.log(hasKeyValuePair({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
-console.log(hasKeyValuePair({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+// console.log(hasKeyValuePair({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+// console.log(hasKeyValuePair({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+
+for (var i = 1; i <= 10; i++) {
+  randomNumber = Math.floor((Math.random() * 10) + 1);
+  arrayOfWords = generateWords(randomNumber);
+  console.log("Of the following words: " + arrayOfWords.toString());
+  console.log(longestPhrase(arrayOfWords) + ' is the longest word.');
+}
